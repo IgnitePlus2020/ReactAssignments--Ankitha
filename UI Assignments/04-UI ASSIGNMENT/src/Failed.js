41ce4712d9e186c454f6b7557ce4e5ee86bb7d2c
Ankitha Chowlur <54694545+ankithac@users.noreply.github.com>
2020-05-06 08:54:23 +0530

    added validation for real-time search

--- UI Assignments/04-UI ASSIGNMENT/src/Failed.js
+++ UI Assignments/04-UI ASSIGNMENT/src/Failed.js
@@ -5,6 +5,21 @@ function Success() {
   return (
     <div className="container">
       <h1> Oops! There is no Customer of the mentioned ID </h1>
+      <br />
+      <h3>Here's a list of Valid ID's you can choose among.. </h3>
+      <br />
+      <table>
+        <tr>
+          {Customers.map(customer => {
+            return (
+              <div>
+                <td>{customer.id}</td>
+                <td>{customer.name}</td>
+              </div>
+            );
+          })}
+        </tr>
+      </table>
     </div>
   );
 }
