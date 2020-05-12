2fb6c40ea6d7484af8b6b1d86ab01c77b243b974
Ankitha Chowlur <54694545+ankithac@users.noreply.github.com>
2020-05-12 11:21:05 +0530

    Update Failed.js

--- UI Assignments/04-UI ASSIGNMENT/src/Failed.js
+++ UI Assignments/04-UI ASSIGNMENT/src/Failed.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Customers from "./Customers";
+import App from "./App";
+import ReactDOM from "react-dom";
 
 function Success() {
+  const [color, setColor] = useState("white");
+  function handleMouseOver() {
+    setColor("black");
+  }
+  function handleMouseOut() {
+    setColor("white");
+  }
+  function handleClick() {
+    ReactDOM.render(<App />, document.getElementById("root"));
+  }
   return (
     <div className="container">
       <h1> Oops! There is no Customer of the mentioned ID </h1>
       <br />
-      <h3>Here's a list of Valid ID's you can choose among.. </h3>
+      <h1>Here's a list of Valid ID's you can choose among.. </h1>
       <br />
-      <table>
+      <table className="container">
         <tr>
           {Customers.map(customer => {
             return (
@@ -20,6 +32,15 @@ function Success() {
           })}
         </tr>
       </table>
+      <button
+        type="submit"
+        style={{ backgroundColor: color }}
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Try Again
+      </button>
     </div>
   );
 }
